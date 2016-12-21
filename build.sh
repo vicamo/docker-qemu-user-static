1045726bb26fd22b2d3781cce1d85559647dda34
You-Sheng Yang (楊有勝) <vicamo@gmail.com>
2016-12-21 11:04:07 +0800

    use lower-cased letters for local variables

--- build.sh
+++ build.sh
@@ -5,19 +5,19 @@ set -xe
 cp /usr/bin/qemu-*-static .
 
 first=$(ls -1 qemu-*-static | head -n 1)
-ARCHES=$(ls -1 qemu-*-static | cut -d- -f2)
-VERSION=$(${first} -version | awk '{print $3}')
-DEBIAN_VERSION=$(dpkg-query -W -f '${Version}' qemu-user-static)
+arches=$(ls -1 qemu-*-static | cut -d- -f2)
+version=$(${first} -version | awk '{print $3}')
+debian_version=$(dpkg-query -W -f '${Version}' qemu-user-static)
 
 echo <<EOF
-ARCHITECTURES: $ARCHES
-VERSION: $VERSION
-DEBIAN_VERSION: $DEBIAN_VERSION
+ARCHITECTURES: ${arches}
+VERSION: ${version}
+DEBIAN_VERSION: ${debian_version}
 EOF
 
 new_version_available=
-for ARCH in ${ARCHES}; do
-  if ! docker pull vicamo/qemu-user-static:${ARCH}-${VERSION}; then
+for arch in ${arches}; do
+  if ! docker pull vicamo/qemu-user-static:${arch}-${version}; then
     new_version_available=yes
     break
   fi
@@ -25,12 +25,12 @@ done
 
 [ -n "${new_version_available}" ] || (echo "No updates available."; exit 0)
 
-for ARCH in ${ARCHES}; do
+for arch in ${arches}; do
   cat Dockerfile.template | \
-    sed "s,@ARCH@,${ARCH},g; s,@VERSION@,${VERSION},g; s,@DEBIAN_VERSION@,${DEBIAN_VERSION},g;" > Dockerfile
-  echo "#### Building $ARCH ####"
+    sed "s,@ARCH@,${arch},g; s,@VERSION@,${version},g; s,@DEBIAN_VERSION@,${debian_version},g;" > Dockerfile
+  echo "#### Building ${arch} ####"
   cat Dockerfile
 
-  docker build -t vicamo/qemu-user-static:$ARCH . \
-  && docker tag vicamo/qemu-user-static:$ARCH vicamo/qemu-user-static:$ARCH-$VERSION
+  docker build -t vicamo/qemu-user-static:${arch} . \
+  && docker tag vicamo/qemu-user-static:${arch} vicamo/qemu-user-static:${arch}-${version}
 done
